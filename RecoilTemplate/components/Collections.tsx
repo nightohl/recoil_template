0b6f19ed26c1c1d9db9d0cca2da42041c467986d
nightohl <haneojin@gmail.com>
2021-05-06 20:18:38 +0900

    feat: Basic Counter - 기본 카운터 예시 리코일로 구현 - 가장 가까운 RecoilRoot를 참조하므로 같은 atoms여도 뿌리가 다르면 값을 공유하지 않음

--- RecoilTemplate/components/Collections.tsx
+++ RecoilTemplate/components/Collections.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import {useRecoilState} from 'recoil';
+import {countState} from '@recoil/atoms/BasicCounter';
 
+const RecoilRootTest = ({onPress}) => {
+  const [count, setCount] = useRecoilState(countState);
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{padding: 10, borderWidth: 1, borderRadius: 10}}>
+          {count}
+        </Text>
+        <Button title="1 감소" onPress={() => setCount(count - 1)} />
+        <Button title="1 증가" onPress={() => setCount(count + 1)} />
+        <Button title="BasicCounter" onPress={onPress} />
+      </View>
+      <Text
+        style={{
+          margin: 8,
+          padding: 10,
+          backgroundColor: 'lightgrey',
+          borderRadius: 15,
+          overflow: 'hidden',
+        }}>
+        컴포넌트는 가장 가까운 RecoilRoot를 참조하므로 동일한 atom을
+        사용하더라도 RecoilRoot가 다르면 공유되지 않는다.
+      </Text>
+    </View>
+  );
+};
 export default function Collections({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button
-        title="BasicCounter"
-        onPress={() => navigation.navigate('BasicCounter')}
-      />
+      <RecoilRootTest onPress={() => navigation.navigate('BasicCounter')} />
     </View>
   );
 }
--- RecoilTemplate/navigations/MyStack.tsx
+++ RecoilTemplate/navigations/MyStack.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {RecoilRoot} from 'recoil';
 import {RootMyStackParamList} from 'Navigation';
+import {countState} from '@recoil/atoms/BasicCounter';
 
 import Collections from '@components/Collections';
 import BasicCounter from '@components/BasicCounter';
@@ -9,9 +11,11 @@ const Stack = createStackNavigator<RootMyStackParamList>();
 
 export default function MyStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Collections" component={Collections} />
-      <Stack.Screen name="BasicCounter" component={BasicCounter} />
-    </Stack.Navigator>
+    <RecoilRoot>
+      <Stack.Navigator>
+        <Stack.Screen name="Collections" component={Collections} />
+        <Stack.Screen name="BasicCounter" component={BasicCounter} />
+      </Stack.Navigator>
+    </RecoilRoot>
   );
 }
