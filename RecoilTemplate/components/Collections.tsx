d0ea1d8d82dd801c1cf826e502ac382bcadbe630
nightohl <haneojin@gmail.com>
2021-05-07 11:57:31 +0900

    feat: ColorBoxes example
    - 총 25개의 블럭
    - red, blue, white 색상을 선택하여 박스별 색상을 별도 상태로 관리
    - 색상별 개수 출력
    - 상태 reset

--- RecoilTemplate/components/Collections.tsx
+++ RecoilTemplate/components/Collections.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import {useRecoilState} from 'recoil';
-import {countState} from '@recoil/atoms/BasicCounter';
+import {countState} from '@recoil/BasicCounter';
 
 const RecoilRootTest = ({onPress}) => {
   const [count, setCount] = useRecoilState(countState);
   return (
-    <View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+    <View style={styles.container}>
+      <View style={styles.basicCounterContainer}>
         <Text style={{padding: 10, borderWidth: 1, borderRadius: 10}}>
           {count}
         </Text>
@@ -20,14 +15,7 @@ const RecoilRootTest = ({onPress}) => {
         <Button title="1 증가" onPress={() => setCount(count + 1)} />
         <Button title="BasicCounter" onPress={onPress} />
       </View>
-      <Text
-        style={{
-          margin: 8,
-          padding: 10,
-          backgroundColor: 'lightgrey',
-          borderRadius: 15,
-          overflow: 'hidden',
-        }}>
+      <Text style={styles.description}>
         컴포넌트는 가장 가까운 RecoilRoot를 참조하므로 동일한 atom을
         사용하더라도 RecoilRoot가 다르면 공유되지 않는다.
       </Text>
@@ -38,6 +26,28 @@ export default function Collections({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <RecoilRootTest onPress={() => navigation.navigate('BasicCounter')} />
+      <View style={styles.container}>
+        <Button
+          title="ColorBoxes"
+          onPress={() => navigation.navigate('ColorBoxes')}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  description: {
+    margin: 8,
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  basicCounterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {borderWidth: 2, padding: 5, width: '100%', marginTop: -2},
+});
